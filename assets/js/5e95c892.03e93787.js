45114a2da4766e7f75964c27d6cb28a1fd3d21c8
lRENyaaa <92320175+lRENyaaa@users.noreply.github.com>
2024-06-08 15:23:48 +0000

    Deploying to gh-pages from @ postyizhan/NitWikit@86ad2bd2cad9cb668c61c15f48233ccece25f1dc 🚀

--- assets/js/5e95c892.03e93787.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkWiki=self.webpackChunkWiki||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>k});r(6540);var u=r(4164),a=r(1003),c=r(7559),i=r(2831),n=r(5476),t=r(4848);function k(e){return(0,t.jsx)(a.e3,{className:(0,u.A)(c.G.wrapper.docsPages),children:(0,t.jsx)(n.A,{children:(0,i.v)(e.route.routes)})})}}}]);
