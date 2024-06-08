c971c47fd6d9d5db24d498977b2a374795735b4a
postyizhan <postyizhan@users.noreply.github.com>
2024-06-08 21:20:42 +0000

    Deploying to gh-pages from @ postyizhan/NitWikit@9a94baec2109c6fda1ecd603590d1946a60845e0 🚀

---
+++ assets/js/5e95c892.b0e87087.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkWiki=self.webpackChunkWiki||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>k});r(96540);var u=r(34164),a=r(1003),c=r(17559),i=r(22831),n=r(25476),t=r(74848);function k(e){return(0,t.jsx)(a.e3,{className:(0,u.A)(c.G.wrapper.docsPages),children:(0,t.jsx)(n.A,{children:(0,i.v)(e.route.routes)})})}}}]);
