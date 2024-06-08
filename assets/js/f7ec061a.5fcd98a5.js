543bfba9f4792a8688bae6f5b3a68af28ef9c4d2
postyizhan <97342038+postyizhan@users.noreply.github.com>
2024-06-08 04:09:12 +0000

    Deploying to gh-pages from @ postyizhan/NitWikit@dcfaf3260e063411127f185120b86268232a1b05 🚀

---
+++ assets/js/f7ec061a.5fcd98a5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkWiki=self.webpackChunkWiki||[]).push([[1882],{65109:e=>{e.exports=JSON.parse('{"title":"\u4f18\u5316","slug":"/category/\u4f18\u5316","permalink":"/category/\u4f18\u5316","navigation":{"previous":{"title":"RNG","permalink":"/\u53cd\u4f5c\u5f0a/rng"},"next":{"title":"\u6982\u89c8","permalink":"/\u4f18\u5316/\u6982\u89c8"}}}')}}]);
