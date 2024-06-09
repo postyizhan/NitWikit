9cc4ba1396c81039bb0cb9aba0e3393be118d3c5
postyizhan <97342038+postyizhan@users.noreply.github.com>
2024-06-09 12:05:38 +0000

    Deploying to gh-pages from @ postyizhan/NitWikit@f4be3348507ce7931bee45cfc5d6dc9dc9ca7f3d 🚀

--- assets/js/b5477f5d.2253e430.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkWiki=self.webpackChunkWiki||[]).push([[1511],{79343:e=>{e.exports=JSON.parse('{"title":"skript","slug":"/category/skript","permalink":"/category/skript","navigation":{"previous":{"title":"\u6570\u5b66\u8fd0\u7b97","permalink":"/sundry/kether/\u6570\u5b66\u8fd0\u7b97"},"next":{"title":"\u6982\u89c8","permalink":"/sundry/skript/\u6982\u89c8"}}}')}}]);
