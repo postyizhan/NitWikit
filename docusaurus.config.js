1bd57d36348a4c1c52a3a8e43bd21049a954fd98
Radiation-pi <96102795+Radiation-pi@users.noreply.github.com>
2024-07-14 10:17:23 +0800

    修复路径有中文

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,7 +72,7 @@ const config = {
       {
         id: 'docs-java',
         path: 'docs-java',
-        routeBasePath: 'Java版',
+        routeBasePath: 'Java',
         editUrl: 'https://github.com/postyizhan/NitWikit/tree/main',
         sidebarPath: require.resolve('./sidebars.js'),
         editCurrentVersion: true,
@@ -85,7 +85,7 @@ const config = {
       {
         id: 'docs-bedrock',
         path: 'docs-bedrock',
-        routeBasePath: '基岩版',
+        routeBasePath: 'Bedrock',
         editUrl: 'https://github.com/postyizhan/NitWikit/tree/main',
         sidebarPath: require.resolve('./sidebars.js'),
         editCurrentVersion: true,
