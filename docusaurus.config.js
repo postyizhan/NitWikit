70b1f27307c0080e76ff88a00321e6d10aa0a84e
“postyizhan” <185839426@qq.com>
2024-08-07 19:01:28 +0800

    algolia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,6 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-
   customFields: {
     // 标题前缀
     titlePrefix: "主页",
@@ -212,6 +211,38 @@ const config = {
       // 颜色随系统切换
       colorMode: {
         respectPrefersColorScheme: true,
+      },
+
+      // 搜索
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'D1KV1BYF3B',
+
+        // Public API key: it is safe to commit it
+        apiKey: '4bb3573e59f2c49f30f057ce54edab3f',
+
+        indexName: 'yizhan',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+        // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+        insights: false,
       },
     }),
 
