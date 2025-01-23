29b6cb8869383a08025e46ce1481ebb6fe93d0c9
lilingfengdev <lilingfengvvv@gmail.com>
2025-01-23 22:01:20 +0800

    启用可持久化 Cache

--- rspack.config.js
+++ rspack.config.js
@@ -1,11 +1,15 @@
 
 const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
+    cache: true,
   experiments: {
       futureDefaults: true,
       asyncWebAssembly: true,
       css: true,
       lazyCompilation: isDev,
       incremental: isDev,
+      cache: {
+          type: 'persistent',
+      },
   },
 };
